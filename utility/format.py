49806a619a5d72be5706431964dd3022d9b36ed5
Dalton Behringer <dalton.behringer@gmail.com>
2024-05-14 12:27:01 -0700

    updated locs

--- utility/format.py
+++ utility/format.py
@@ -18,37 +18,38 @@ def concat_data(
         for filename in glob.glob('aqpi*PPI.netcdf*'):
                 f = fdir + filename
                 nc = Dataset(f, 'r')
-        
-        fdir = '/Users/daltonbehringer/MTR_data/xband/XEBY/2024/Feb19/'
-        f = fdir + 'aqpi.xeby-20240219-224651_3035_2_4_PPI.netcdf.drops'
-        nc = Dataset(f, 'r')
 
-        scan_time = nc.time_coverage_end
+                scan_time = nc.time_coverage_end
 
-        refl = nc.variables['CorrReflectivity'][:]
-        zdr = nc.variables['CorrDifferentialReflectivity'][:]
-        #snr = nc.variables['SignalToNoiseRatio'][:]
-        elev = np.round(nc.variables['elevation'][:][1], decimals=0)
-        gw = nc.variables['gateWidth'][:]
-        #gw = gw/1000
-        az = nc.variables['azimuth'][:]
-        fg = nc.variables['startRange'][:]
-        fg = (fg*-1)
+                refl = nc.variables['CorrReflectivity'][:]
+                zdr = nc.variables['CorrDifferentialReflectivity'][:]
+                cc = nc.variables['CrossPolCorrelation'][:]
+                kdp = nc.variables['KDP'][:]
+                vel = nc.variables['Velocity'][:]
+                elev = np.round(nc.variables['elevation'][:][1], decimals=0)
+                gw = nc.variables['gateWidth'][:]
+                #gw = gw/1000
+                az = nc.variables['azimuth'][:]
+                fg = nc.variables['startRange'][:]
+                fg = (fg*-1)
 
-        refl[zdr < 0.] = np.nan
-        zdr[zdr < 0.] = np.nan
+                refl[zdr < 0.] = np.nan
+                zdr[zdr < -8.] = np.nan
+                cc[cc < 0.] = np.nan
+                kdp[kdp <-2.] = np.nan
+                vel[vel < -99.] = np.nan
 
-        refl = np.ma.array(refl, mask=np.isnan(refl))
+                refl = np.ma.array(refl, mask=np.isnan(refl))
 
-        gates = np.arange(1, refl.shape[1]+1)
-        gates = (gates*gw[1])
-        g1, az1 = np.meshgrid(gates, az)
+                gates = np.arange(1, refl.shape[1]+1)
+                gates = (gates*gw[1])
+                g1, az1 = np.meshgrid(gates, az)
 
-        g1, fg1 = np.meshgrid(gates, fg)
-        r = g1+fg1
+                g1, fg1 = np.meshgrid(gates, fg)
+                r = g1+fg1
 
-        x = r * np.cos(np.radians(az1-90))
-        y = -r * np.sin(np.radians(az1-90))
+                x = r * np.cos(np.radians(az1-90))
+                y = -r * np.sin(np.radians(az1-90))
 
 
 
--- utility/radar_locs.py
+++ utility/radar_locs.py
@@ -5,14 +5,14 @@ Returns lat, lon of radar for plotting functions
 def get_loc(var):
 
 	if var is 'XSCW':
-		lon, lat = -122.783451, 38.511884
+		lon, lat = -122.802292, 38.521615
 	if var is 'XEBY':
-		lon, lat = -122.062316, 37.815769
-	if var is 'XEBY2':
-		lon, lat = -122.422901, 37.574263
+		lon, lat = -122.061983, 37.815557
+	# if var is 'XEBY2':
+	# 	lon, lat = -122.422901, 37.574263
 	if var is 'XSCV':
 		lon, lat = -121.833361, 37.398870
 	if var is 'XSCR':
-		lon, lat = -121.978596, 36.984753
+		lon, lat = -121.978641, 36.984709
 
 	return lon, lat
